7f45fb90c65aa2f74cbe8a17438a429018f44b80
ismael3s <souz4ismael@gmail.com>
2023-08-05 22:12:38 -0300

    feat: addednew social media

--- src/app/[locale]/components/About/SocialMedia.tsx
+++ src/app/[locale]/components/About/SocialMedia.tsx
@@ -2,25 +2,36 @@
 import Image from "next/image";
 import GithubSvg from "../../.././../../public/Github.svg";
 import LinkedinSvg from "../../.././../../public/Linkedin.svg";
+import { BiLogoDevTo, BiLogoGmail } from "react-icons/bi";
 export const SocialMedia = () => {
-  function openInNewTab(url: string) {
-    const win = window.open(url, "_blank");
+  function openURL(url: string, target="_blank") {
+    const win = window.open(url, target);
     win?.focus();
   }
 
+
   return (
-    <div className="flex gap-2">
+    <div className="flex gap-4">
       <Image
         src={GithubSvg}
         alt="Github Logo"
-        onClick={() => openInNewTab("https://github.com/ismael3s")}
+        onClick={() => openURL("https://github.com/ismael3s")}
         className="cursor-pointer hover:opacity-80 transition-all duration-300 ease-in-out"
       />
       <Image
         src={LinkedinSvg}
         alt="Linkedin Logo"
-        onClick={() => openInNewTab("https://linkedin.com/in/ismael3s")}
+        onClick={() => openURL("https://linkedin.com/in/ismael3s")}
         className="cursor-pointer hover:opacity-80 transition-all duration-300 ease-in-out"
+      />
+      <BiLogoGmail
+        onClick={() => openURL("mailto:ismael.santana.dev@gmail.com", "_self")}
+        className="w-6 h-6 cursor-pointer hover:opacity-80 transition-all duration-300 ease-in-out"
+      />
+
+      <BiLogoDevTo
+        onClick={() => openURL("https://dev.to/ismael3s")}
+        className="w-6 h-6 cursor-pointer hover:opacity-80 transition-all duration-300 ease-in-out"
       />
     </div>
   );
